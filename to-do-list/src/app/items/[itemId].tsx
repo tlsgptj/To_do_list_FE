de6318fafc13b641d07f69f742fd69a5327990b1
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-10-11 20:57:33 +0900

    아이템 파일 수정

---
+++ to-do-list/src/app/items/[itemId].tsx
@@ -0,0 +1,7 @@
+import dynamic from 'next/dynamic';
+
+const TodoDetailPage = dynamic(() => import('../components/TodoDetailPage'), { ssr: false });
+
+export default function ItemPage() {
+  return <TodoDetailPage />;
+}
--- to-do-list/src/app/items/[itemId]/Detail.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic';
-
-// dynamic import를 사용하여 클라이언트 전용 컴포넌트를 불러옵니다.
-const TodoDetailPage = dynamic(() => import('../../components/TodoDetailPage'), { ssr: false });
-
-export default function ItemPage() {
-  return <TodoDetailPage />;
-}
