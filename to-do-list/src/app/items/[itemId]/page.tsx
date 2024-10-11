85407b3798787fb1c5eb4021a5dd7bbf0e7d634e
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-10-11 21:37:36 +0900

    세부사항 로드 성공

--- to-do-list/src/app/items/[itemId]/page.tsx
+++ to-do-list/src/app/items/[itemId]/page.tsx
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic';
 
-const TodoDetailPage = dynamic(() => import('../components/TodoDetailPage'), { ssr: false });
+const TodoDetailPage = dynamic(() => import('../../components/TodoDetailPage'), { ssr: false });
 
 export default function ItemPage() {
   return <TodoDetailPage />;
