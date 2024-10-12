13d6e44e634d086bb78af735e147870a1f542955
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-10-12 16:20:50 +0900

    page.tsx수정

--- to-do-list/src/app/page.tsx
+++ to-do-list/src/app/page.tsx
@@ -1,20 +1,15 @@
-import Image from 'next/image';
 import TodoList from './components/TodoList';
+import Layout from './components/Layout';
 
 export default function HomePage() {
   return (
-    <main className="flex flex-col items-start justify-start mt-2">
-      <div className="flex items-center space-x-2 mt-2"> 
-        <Image
-          src="/images/logo.png" 
-          alt="Logo"
-          width={50} 
-          height={50}
-        />
-        <h1 className="text-2xl font-bold" style={{ color: '#4B0082' }}>do it;</h1>
-      </div>
-      <TodoList />
-    </main>
+    <div>
+      <Layout />
+      <main>
+        <TodoList />
+      </main>
+    </div>
   );
 }
+
 
