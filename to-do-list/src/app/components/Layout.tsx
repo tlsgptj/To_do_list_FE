12687e178a11c043e31319e4407cb72b81baa203
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-10-12 16:35:23 +0900

    로고 클릭 시 초기화

--- to-do-list/src/app/components/Layout.tsx
+++ to-do-list/src/app/components/Layout.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 
-const Layout: React.FC = () => {
+interface LayoutProps {
+  onReset: () => void; 
+}
+
+const Layout: React.FC<LayoutProps> = ({ onReset }) => {
   return (
-    <div className="flex items-center space-x-2 mt-2"> 
-        <img
-          src="/images/logo.png" 
-          alt="Logo"
-          width={50} 
-          height={50}
-        />
-        <h1 className="text-2xl font-bold" style={{ color: '#4B0082' }}>do it;</h1>
-      </div>
+    <div className="flex items-center space-x-2 mt-2" onClick={onReset}> {/* 클릭 시 onReset 호출 */}
+      <img
+        src="/images/logo.png"
+        alt="Logo"
+        width={50}
+        height={50}
+      />
+      <h1 className="text-2xl font-bold" style={{ color: '#4B0082' }}>do it;</h1>
+    </div>
   );
 };
 
 export default Layout;
+
